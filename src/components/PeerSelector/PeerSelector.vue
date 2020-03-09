94ced9814c0a2e70a158e3cad5c14b3b3aceae5d
decentraliser <decentraliser@gmail.com>
2020-03-09 12:38:36 +0800

    lint

--- src/components/PeerSelector/PeerSelector.vue
+++ src/components/PeerSelector/PeerSelector.vue
@@ -1,58 +1,59 @@
 <template>
-<div :class="[isConnected ? 'endpoint-healthy' : 'endpoint-unhealthy']">
-  <Poptip placement="bottom-end">
-    <i class="pointer point" />
-    <span v-if="isConnected" class="network_type_text">
-    {{ networkTypeText }}
-    </span>
-    <div slot="title" class="title">
-    {{ $t('current_endpoint') }}：{{ currentPeer.url }}
-    </div>
-    <div slot="content">
-      <div class="node_list">
-        <div class="node_list_container scroll">
-          <div
+  <div :class="[isConnected ? 'endpoint-healthy' : 'endpoint-unhealthy']">
+    <Poptip placement="bottom-end">
+      <i class="pointer point" />
+      <span v-if="isConnected" class="network_type_text">
+        {{ networkTypeText }}
+      </span>
+      <div slot="title" class="title">
+        {{ $t('current_endpoint') }}：{{ currentPeer.url }}
+      </div>
+      <div slot="content">
+        <div class="node_list">
+          <div id="node-list-container" class="node_list_container scroll">
+            <div
               v-for="(nodeUrl, index) in peersList"
               :key="`sep${index}`"
               class="point_item pointer"
               @click="switchPeer(nodeUrl)"
-          >
-            <img :src="currentPeer.url === nodeUrl ? imageResources.selected : imageResources.unselected">
-            <span class="node_url">{{ nodeUrl }}</span>
-            <img
-              class="remove_icon" src="@/views/resources/img/service/multisig/multisigDelete.png"
-              @click.stop="removePeer(nodeUrl)">
-          </div>
-        </div>
-
-        <form
-          class="input_point point_item"
-          action="submit"
-          onsubmit="event.preventDefault()"
-          @keyup.enter="addPeer"
-        >
-              <!-- v-validate="validationRules.friendlyNodeUrl" -->
-
-          <ErrorTooltip placement-override="top" class="node-input-container" field-name="friendlyNodeUrl">
-            <input
-              v-model="formItems.nodeUrl"
-              :data-vv-as="$t('node')"
-              data-vv-name="friendlyNodeUrl"
-              :placeholder="$t('please_enter_a_custom_nod_address')"
+            >
+              <img :src="currentPeer.url === nodeUrl ? imageResources.selected : imageResources.unselected">
+              <span class="node_url">{{ nodeUrl }}</span>
+              <img
+                class="remove_icon" src="@/views/resources/img/service/multisig/multisigDelete.png"
+                @click.stop="removePeer(nodeUrl)"
               >
-          </ErrorTooltip>
-          <span class="sure_button radius pointer" @click="addPeer">+</span>
-          <span
-            class="reset-icon radius pointer"
-            @click.stop="resetList()"
+            </div>
+          </div>
+
+          <form
+            class="input_point point_item"
+            action="submit"
+            onsubmit="event.preventDefault()"
+            @keyup.enter="addPeer"
           >
-            <Icon type="md-refresh" />
-          </span>
-        </form>
+            <!-- v-validate="validationRules.friendlyNodeUrl" -->
+
+            <ErrorTooltip placement-override="top" class="node-input-container" field-name="friendlyNodeUrl">
+              <input
+                v-model="formItems.nodeUrl"
+                :data-vv-as="$t('node')"
+                data-vv-name="friendlyNodeUrl"
+                :placeholder="$t('please_enter_a_custom_nod_address')"
+              >
+            </ErrorTooltip>
+            <span class="sure_button radius pointer" @click="addPeer">+</span>
+            <span
+              class="reset-icon radius pointer"
+              @click.stop="resetList()"
+            >
+              <Icon type="md-refresh" />
+            </span>
+          </form>
+        </div>
       </div>
-    </div>
-  </Poptip>
-</div>
+    </Poptip>
+  </div>
 </template>
 
 <script lang="ts">
