744cc4aa97623a822cef9907bed9ba0fc36fe4e0
vincent90 <6521804+vincent_lee90@user.noreply.gitee.com>
2020-03-13 19:10:42 +0800

    cancel the hidden when the wordsArray'length over 24

--- src/components/MnemonicInput/MnemonicInput.vue
+++ src/components/MnemonicInput/MnemonicInput.vue
@@ -2,7 +2,7 @@
     <div v-click-focus class="show-mnemonic">
         <div class="input-already" v-for="word in wordsArray">{{word}}</div>
         <div class="mnemonic-input-container">
-            <input v-show="wordsArray.length<=24" class="mnemonic-input" v-on:keyup.space='addWord' v-on:keyup.delete="deleteWord"
+            <input  class="mnemonic-input" v-on:keyup.space='addWord' v-on:keyup.delete="deleteWord"
                 v-model.trim="inputWord" type="text" maxlength="50">
         </div>
     </div>
