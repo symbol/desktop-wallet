4d396191e05319e2df628cd2e5a945b6b53e82eb
vincent90 <6521804+vincent_lee90@user.noreply.gitee.com>
2020-03-16 10:11:33 +0800

    enhance the mnemonic input

--- src/components/MnemonicInput/MnemonicInput.vue
+++ src/components/MnemonicInput/MnemonicInput.vue
@@ -1,6 +1,6 @@
 <template>
     <div v-click-focus class="show-mnemonic">
-        <div class="input-already" v-for="word in wordsArray">{{word}}</div>
+        <div class="input-already" v-for="word in wordsArray" :key="word">{{word}}</div>
         <div class="mnemonic-input-container">
             <input  class="mnemonic-input" v-on:keyup.space='addWord' v-on:keyup.delete="deleteWord"
                 v-model.trim="inputWord" type="text" maxlength="50">
