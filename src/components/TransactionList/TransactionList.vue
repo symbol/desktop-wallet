BenaYee/symbol-desktop-wallet

dashboard homepage(#183)
  *enhance the style of dashboard homePage

  *adjust the order of transactions

  *remove the tab pane in dashboard transactions list

  *fixed the spin loading

  *fixed the display of message when there is no transactions

fix issue#187

  *Optimized asset list scrollbar flash

  *Optimized Mosaic list scrollbar style

src/components/TransactionList/TransactionList.vue
@@ -2,60 +2,13 @@
   <div class="transaction-list-outer-container">
     <div class="transaction-list-inner-container">
       <div class="transaction-list-tabs-container">
-        <Tabs
-          v-model="currentTab"
-          :active-key="currentTab"
-          class="transaction-list-tabs"
-          @input="onTabChange"
-        >
-          <TabPane
-            :label="$t('transactions_tab_confirmed')"
-            :tab="'confirmed'"
-            :name="'confirmed'"
-            :icon="''"
-            class="transaction-tab-inner-container"
-            @input="currentTab = 'confirmed'"
-          >
-            <!-- Confirmed transactions tab -->
-            <TransactionTable
-              :transactions="getCurrentPageTransactions('confirmed')"
-              :empty-message="'no_confirmed_transactions'"
-              @click="onClickTransaction"
-            />
-          </TabPane>
-          <TabPane
-            :label="$t('transactions_tab_unconfirmed')"
-            :tab="'unconfirmed'"
-            :name="'unconfirmed'"
-            :icon="''"
-            @input="currentTab = 'unconfirmed'"
-          >
-            <!-- Unconfirmed transactions tab -->
-            <TransactionTable
-              :transactions="getCurrentPageTransactions('unconfirmed')"
-              :empty-message="'no_unconfirmed_transactions'"
-              @click="onClickTransaction"
-            />
-          </TabPane>
-          <TabPane
-            :label="$t('transactions_tab_partial')"
-            :tab="'partial'"
-            :name="'partial'"
-            :icon="''"
-            @input="currentTab = 'partial'"
-          >
-            <!-- Partial transactions tab -->
-            <TransactionTable
-              :transactions="getCurrentPageTransactions('partial')"
-              :empty-message="'no_partial_transactions'"
-              @click="onClickTransaction"
-            />
-          </TabPane>
-          <TransactionListOptions slot="extra" :current-tab="currentTab" />
-        </Tabs>
+        <TransactionListOptions @option-change="getTransactionListByOption" />
+        <TransactionTable
+          :transactions="getCurrentPageTransactions()" :empty-message="getEmptyMessage()"
+          @click="onClickTransaction"
+        />
       </div>
       <div class="transaction-list-pagination-container">
-        <!-- <span>{{ $t('total_transactions') }}：{{ totalCountItems }}</span> -->
         <Page
           :total="totalCountItems"
           class="page_content"

symbol/desktop-wallet

  *move the TransactionListOptions folder to transactionList,renamed TransactionListOptions to TransactionListFilters and add 2 folders

src/components/TransactionList/TransactionList.vue
@@ -2,60 +2,13 @@
   <div class="transaction-list-outer-container">
     <div class="transaction-list-inner-container">
       <div class="transaction-list-tabs-container">
-        <Tabs
-          v-model="currentTab"
-          :active-key="currentTab"
-          class="transaction-list-tabs"
-          @input="onTabChange"
-        >
-          <TabPane
-            :label="$t('transactions_tab_confirmed')"
-            :tab="'confirmed'"
-            :name="'confirmed'"
-            :icon="''"
-            class="transaction-tab-inner-container"
-            @input="currentTab = 'confirmed'"
-          >
-            <!-- Confirmed transactions tab -->
-            <TransactionTable
-              :transactions="getCurrentPageTransactions('confirmed')"
-              :empty-message="'no_confirmed_transactions'"
-              @click="onClickTransaction"
-            />
-          </TabPane>
-          <TabPane
-            :label="$t('transactions_tab_unconfirmed')"
-            :tab="'unconfirmed'"
-            :name="'unconfirmed'"
-            :icon="''"
-            @input="currentTab = 'unconfirmed'"
-          >
-            <!-- Unconfirmed transactions tab -->
-            <TransactionTable
-              :transactions="getCurrentPageTransactions('unconfirmed')"
-              :empty-message="'no_unconfirmed_transactions'"
-              @click="onClickTransaction"
-            />
-          </TabPane>
-          <TabPane
-            :label="$t('transactions_tab_partial')"
-            :tab="'partial'"
-            :name="'partial'"
-            :icon="''"
-            @input="currentTab = 'partial'"
-          >
-            <!-- Partial transactions tab -->
-            <TransactionTable
-              :transactions="getCurrentPageTransactions('partial')"
-              :empty-message="'no_partial_transactions'"
-              @click="onClickTransaction"
-            />
-          </TabPane>
-          <TransactionListOptions slot="extra" :current-tab="currentTab" />
-        </Tabs>
+        <TransactionListFilters @option-change="getTransactionListByOption" />
+        <TransactionTable
+          :transactions="getCurrentPageTransactions()" :empty-message="getEmptyMessage()"
+          @click="onClickTransaction"
+        />
       </div>
       <div class="transaction-list-pagination-container">
-        <!-- <span>{{ $t('total_transactions') }}：{{ totalCountItems }}</span> -->
         <Page
           :total="totalCountItems"
           class="page_content"