a6014acd6cde618215850791c13b442f25ed0842
decentraliser <decentraliser@gmail.com>
2020-04-27 18:34:11 +0800

    change user data storage folder location

--- public/build.js
+++ public/build.js
@@ -3,6 +3,10 @@ const path = require('path')
 const {app, BrowserWindow, shell, globalShortcut, Menu, ipcMain} = require('electron')
 const electron = require('electron')
 const name = electron.app.getName()
+
+// Set the path of the folder where the persisted data is stored
+electron.app.setPath ('userData', path.join(electron.app.getPath('home'), '.symbol-desktop-wallet'))
+
 const iconUrlPath = process.platform === 'darwin' ? './dist/assets/logo.png' : '../dist/assets/logo.png'
 const loadUrlPath = process.platform === 'darwin' ? './dist/index.html' : '../dist/index.html'
 
