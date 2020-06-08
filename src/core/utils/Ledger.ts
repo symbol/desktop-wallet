1a99dcd83d7d87bc0cd678d26be31b2403d22953
NGUYEN Thanh Hang <hang.nguyen@fdssoft.com>
2020-06-08 09:41:01 +0700

    update code ledger with new version

--- src/core/utils/Ledger.ts
+++ src/core/utils/Ledger.ts
@@ -96,9 +96,8 @@ export class SymbolLedger {
     let twiceTransfer
     // The length of the APDU buffer is 255Bytes
     if (rawTx.length > 446) {
-      console.log('Length of rawTx is over than 446')
       app.$Notice.error({
-        title: 'Transaction length is over the limit.' + '',
+        title: this['$t']('Transaction length is over the limit.') + '',
       })
     } else {
       twiceTransfer = rawTx.length > 234 ? true : false
