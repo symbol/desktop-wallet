67ee4102415f2d96b3064475a8c3d0118ceb5404
thanhhang-fds <hang.nguyen@fdssoft.com>
2020-06-29 11:49:08 +0700

    fix conflict

--- src/views/forms/FormPersistentDelegationRequestTransaction/FormPersistentDelegationRequestTransactionTs.ts
+++ src/views/forms/FormPersistentDelegationRequestTransaction/FormPersistentDelegationRequestTransactionTs.ts
@@ -156,6 +156,6 @@ export class FormPersistentDelegationRequestTransactionTs extends FormTransactio
 
     // - open signature modal
     this.command = this.createTransactionCommand()
-    return this.command
+    return (this.command as unknown) as Promise<void>
   }
 }
