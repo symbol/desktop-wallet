905e1a1ce0375661856946f024796760f4d43229
Bassem <bassem.magdyy94@gmail.com>
2020-07-20 15:47:03 +0200

    update url

--- src/components/AccountLinks/AccountLinksTs.ts
+++ src/components/AccountLinks/AccountLinksTs.ts
@@ -43,6 +43,6 @@ export class AccountLinksTs extends Vue {
   }
 
   public get faucetUrl() {
-    return this.faucetBaseUrl+ '?recipient="' + this.account.address + '"'
+    return this.faucetBaseUrl+ '?recipient=' + this.account.address
   }
 }
