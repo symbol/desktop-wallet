0ba4bcac2210019b2a41881ae3b0e3750d03cd4a
Bassem <bassem.magdyy94@gmail.com>
2020-08-14 15:57:30 +0200

    allow msg only transactions without mosaics #474

--- src/views/forms/FormTransferTransaction/FormTransferTransaction.vue
+++ src/views/forms/FormTransferTransaction/FormTransferTransaction.vue
@@ -22,7 +22,7 @@
               :mosaic-hex-ids="mosaicInputsManager.getMosaicsBySlot(attachedMosaic.uid)"
               :absolute="false"
               :uid="attachedMosaic.uid"
-              :is-show-delete="index > 0 && index === formItems.attachedMosaics.length - 1"
+              :is-show-delete="index >= 0 && index === formItems.attachedMosaics.length - 1"
               :is-first-item="index === 0"
               @input-changed="onMosaicInputChange"
               @input-deleted="onDeleteMosaicInput"
