f21d645b858bf73ec7cf87d6a9958c4ca68a1a10
Baha <bahadir.yilmaz@nem.software>
2020-10-14 11:55:55 +0100

    Transaction list rest pagination with infinite scroll & conventional pagination, fixes #532

--- src/components/TransactionList/TransactionTable/TransactionTableTs.ts
+++ src/components/TransactionList/TransactionTable/TransactionTableTs.ts
@@ -7,6 +7,7 @@ import { mapGetters } from 'vuex'
 import TransactionRow from '@/components/TransactionList/TransactionRow/TransactionRow.vue'
 // @ts-ignore
 import TransactionListHeader from '@/components/TransactionList/TransactionListHeader/TransactionListHeader.vue'
+import { PageInfo } from '@/store/Transaction'
 
 @Component({
   components: {
@@ -15,6 +16,7 @@ import TransactionListHeader from '@/components/TransactionList/TransactionListH
   },
   computed: mapGetters({
     isFetchingTransactions: 'transaction/isFetchingTransactions',
+    currentConfirmedPage: 'transaction/currentConfirmedPage',
   }),
 })
 export class TransactionTableTs extends Vue {
@@ -24,6 +26,12 @@ export class TransactionTableTs extends Vue {
   @Prop({ default: 'no_data_transactions' })
   public emptyMessage: string
 
+  @Prop({ default: null })
+  public loadMore: () => void
+
+  @Prop({ default: 'pagination' })
+  public paginationType!: 'pagination' | 'scroll'
+
   public nodata = [...Array(10).keys()]
 
   /**
@@ -32,4 +40,25 @@ export class TransactionTableTs extends Vue {
    * @type {boolean}
    */
   protected isFetchingTransactions: boolean
+
+  /**
+   * Current confirmed page info
+   * @see {Transaction.PageInfo}
+   * @var {PageInfo}
+   */
+  public currentConfirmedPage: PageInfo
+
+  /**
+   * Whether infinite scroll is currently disabled
+   */
+  protected get infiniteScrollDisabled() {
+    return this.paginationType !== 'scroll' || this.isFetchingTransactions
+  }
+
+  /**
+   * Whether it is currently fetching more transactions from repository
+   */
+  protected get isFetchingMoreTransctions(): boolean {
+    return this.isFetchingTransactions && this.currentConfirmedPage.pageNumber > 1
+  }
 }
