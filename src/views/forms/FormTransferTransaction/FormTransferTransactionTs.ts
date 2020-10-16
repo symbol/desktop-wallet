b40880cb639dc37f05244ac74ce8116d99ab3f14
Bassem <bassem.magdyy94@gmail.com>
2020-10-16 11:24:23 +0200

    update license

--- src/views/forms/FormTransferTransaction/FormTransferTransactionTs.ts
+++ src/views/forms/FormTransferTransaction/FormTransferTransactionTs.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 NEM Foundation (https://nem.io)
+ * Copyright 2020-present NEM (https://nem.io)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
