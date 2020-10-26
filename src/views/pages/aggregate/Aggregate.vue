3ed4853bb0a5c3fbb3f7d43b2384f570bbcb60f1
uridr <orioldomingoroig@gmail.com>
2020-10-26 21:09:33 +0100

    Enlarge central cards in layout. Aggregate view cards and transaction type style done.

--- src/views/pages/aggregate/Aggregate.vue
+++ src/views/pages/aggregate/Aggregate.vue
@@ -1,5 +1,61 @@
 <template>
     <div class="main-container">
+            <div class="lef-container">
+                <div class="left-top-container">
+                    <span class="title-container">
+                        left-top
+                    </span>     
+                    <div class="mosaic_data">
+                        <span class="img_container">
+                            <img src="@/views/resources/img/symbol/XYMCoin.png" alt />
+                        </span>
+                        <span class="mosaic_name">Simple transfer</span>
+                        <span class="add_button">
+                            <img src="@/views/resources/img/symbol/XYMCoin.png" alt />
+                        </span>
+                    </div>
+                     <div class="mosaic_data">
+                        <span class="img_container">
+                            <img src="@/views/resources/img/symbol/XYMCoin.png" alt />
+                        </span>
+                        <span class="mosaic_name">Mosaic transaction</span>
+                        <span class="add_button">
+                            <img src="@/views/resources/img/symbol/XYMCoin.png" alt />
+                        </span>                    </div>
+                     <div class="mosaic_data">
+                        <span class="img_container">
+                            <img src="@/views/resources/img/symbol/XYMCoin.png" alt />
+                        </span>
+                        <span class="mosaic_name">Namespace transaction</span>
+                        <span class="add_button">
+                            <img src="@/views/resources/img/symbol/XYMCoin.png" alt />
+                        </span>                    </div>
+                     <div class="mosaic_data">
+                        <span class="img_container">
+                            <img src="@/views/resources/img/symbol/XYMCoin.png" alt />
+                        </span>
+                        <span class="mosaic_name">Multisignature transaction</span>
+                        <span class="add_button">
+                            <img src="@/views/resources/img/symbol/XYMCoin.png" alt />
+                        </span>                    </div>
+                </div>
+                <div class="left-bottom-container">
+                    <span class="title-container">
+                        left-bottom
+                    </span>  
+                </div>
+            </div>
+            <div class="right-container">
+                <span class="title-container">
+                    right
+                </span>
+            </div>
+    </div>
+</template>
+
+
+<!--<template>
+    <div class="main-container">
         <div class="inner-container">
             <div class="top-container">
                 <span class="page-title">
@@ -11,7 +67,7 @@
             </div>
         </div>
     </div>
-</template>
+</template>-->
 
 <script lang="ts">
 // external dependencies
