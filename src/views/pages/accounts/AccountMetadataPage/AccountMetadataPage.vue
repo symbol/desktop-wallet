595e16c3f91b652f2cc3dcd8471c2049ce1a611f
james-lin71 <jameslin848@gmail.com>
2020-10-27 19:59:44 -0300

    add account metadata components

---
+++ src/views/pages/accounts/AccountMetadataPage/AccountMetadataPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="account-detail-outer-container">
+        <div class="account-detail-inner-container">
+            <div class="account-actions-container">
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { AccountMetadataPageTs } from './AccountMetadataPageTs';
+
+export default class AccountMetadataPage extends AccountMetadataPageTs {}
+</script>
+
+<style lang="less" scoped>
+@import './AccountMetadataPage.less';
+</style>
