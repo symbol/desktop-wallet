f01feb4abadd099f2f0889cb923ac61c69a53ff3
uridr <orioldomingoroig@gmail.com>
2020-10-27 18:45:29 +0100

    Aggregate view ready for logic dev.

--- src/views/pages/aggregate/Aggregate.vue
+++ src/views/pages/aggregate/Aggregate.vue
@@ -7,40 +7,43 @@
                             left-top
                         </span>   
                     </div> 
+                    <!--- TODO: ADD onClick Action to all add_button-->
                     <div class="transaction-type-container"> 
                         <div class="mosaic_data">
                             <span class="img_container">
-                                <img src="@/views/resources/img/symbol/XYMCoin.png" alt />
+                                <img src="@/views/resources/img/icons/transactions.svg" alt />
                             </span>
                             <span class="mosaic_name">Simple transfer</span>
-                            <span class="add_button">
-                                <img src="@/views/resources/img/symbol/XYMCoin.png" alt />
-                            </span>
+                            <button class="add_button">
+                                <img src="@/views/resources/img/icons/add.svg" alt />
+                            </button>
                         </div>
-                         <div class="mosaic_data">
+                        <div class="mosaic_data">
                             <span class="img_container">
-                                <img src="@/views/resources/img/symbol/XYMCoin.png" alt />
+                                <img src="@/views/resources/img/icons/mosaic.svg" alt />
                             </span>
                             <span class="mosaic_name">Mosaic transaction</span>
-                            <span class="add_button">
-                                <img src="@/views/resources/img/symbol/XYMCoin.png" alt />
-                            </span>                    </div>
-                         <div class="mosaic_data">
+                            <button class="add_button">
+                                <img src="@/views/resources/img/icons/add.svg" alt />
+                            </button>                    
+                        </div>
+                        <div class="mosaic_data">
                             <span class="img_container">
-                                <img src="@/views/resources/img/symbol/XYMCoin.png" alt />
+                                <img src="@/views/resources/img/icons/namespace.svg" alt />
                             </span>
                             <span class="mosaic_name">Namespace transaction</span>
-                            <span class="add_button">
-                                <img src="@/views/resources/img/symbol/XYMCoin.png" alt />
-                            </span>                    </div>
-                         <div class="mosaic_data">
+                            <button class="add_button">
+                                <img src="@/views/resources/img/icons/add.svg" alt />
+                            </button>                    
+                        </div>
+                        <div class="mosaic_data">
                             <span class="img_container">
-                                <img src="@/views/resources/img/symbol/XYMCoin.png" alt />
+                                <img src="@/views/resources/img/icons/multisig.svg" alt />
                             </span>
                             <span class="mosaic_name">Multisignature transaction</span>
-                            <span class="add_button">
-                                <img src="@/views/resources/img/symbol/XYMCoin.png" alt />
-                            </span>                    
+                            <button class="add_button">
+                                <img src="@/views/resources/img/icons/add.svg" alt />
+                            </button>                    
                         </div>
                     </div>
                 </div>
@@ -50,52 +53,46 @@
                             left-bottom
                         </span>   
                     </div>   
-                    <div class="transaction-type-container">
+                    <!-- TODO: DYNAMIC TRANSACTION LIST & REMOVE onClick Action to all remove_button -->
+                    <div class="transaction-list-container">
                         <div class="transfer_data">
                             <span class="img_container">
-                                <img src="@/views/resources/img/symbol/XYMCoin.png" alt />
+                                <img src="@/views/resources/img/icons/transactions.svg" alt />
                             </span>
                             <span class="transfer_name">asdfdsagfdga</span>
-                            <span class="remove_button">
-                                <img src="@/views/resources/img/symbol/XYMCoin.png" alt />
-                            </span> 
+                            <button class="remove_button">
+                                <img src="@/views/resources/img/icons/bin.svg" alt />
+                            </button> 
                         </div>
+                    </div>
+                    <div class="bottom-container">
+                        <button class="inverted-button pl-2 pr-2" style="cursor: pointer;">
+                            SEND AGGREGATE
+                        </button>
                     </div>
                 </div>
             </div>
             <div class="right-container">
                 <div class="title-container">
-                        <span class="title-style">
-                            right
-                        </span>   
+                    <span class="title-style">
+                        right
+                    </span>  
+                    <div class="transaction-form">
+                        <div>
+                            <!-- TODO: TRANSFER FORM GOES HERE Same as Dashboard Transfer-->
+                            <AggregateTransaction />
+                        </div>
+                    </div> 
                 </div>  
             </div>
     </div>
 </template>
 
-
-<!--<template>
-    <div class="main-container">
-        <div class="inner-container">
-            <div class="top-container">
-                <span class="page-title">
-                    {{ $t('latest_news_articles') }}
-                </span>
-            </div>
-            <div class="bottom-container">
-                <AggregateTransaction />
-            </div>
-        </div>
-    </div>
-</template>-->
-
 <script lang="ts">
 // external dependencies
 import { Component, Vue } from 'vue-property-decorator';
-
 // child components
 import AggregateTransaction from './aggregateTransaction/AggregateTransaction.vue';
-
 @Component({ components: { AggregateTransaction } })
 export default class Community extends Vue {}
 </script>
