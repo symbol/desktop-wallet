90ed3490fd430cc3ea2645354172528bb7d3ee6b
Baha <bahadir.yilmaz@nem.software>
2020-11-02 16:28:12 +0000

    white screen issue on electron apps fixed (vue config changed)

--- public/build.js
+++ public/build.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path')
 const { app, BrowserWindow, shell, globalShortcut, Menu, ipcMain } = require('electron')
