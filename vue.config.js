2d5080c8efd17cae8c12d00e4e68128d6462957f
yilmazbahadir <yilmazbahadir@gmail.com>
2020-11-02 16:46:54 +0000

    electron app white screen issue (#616)
    * release failure fixed
    
    * white screen issue on electron apps fixed (vue config changed)
    
    Co-authored-by: Baha <bahadir.yilmaz@nem.software>

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable prettier/prettier */
+
 const webpack = require('webpack');
 
 const packageVersion = JSON.stringify(require('./package.json').version);
@@ -5,17 +8,66 @@ const web = process.env.WEB || false;
 
 console.log(`Building package ${packageVersion} for Web: ${web}`);
 
+const path = require('path')
+
 module.exports = {
-    configureWebpack: () => {
-        return {
-            plugins: [
-                new webpack.DefinePlugin({
-                    'process.env': {
-                        PACKAGE_VERSION: packageVersion,
-                        WEB: web,
-                    },
-                }),
-            ],
-        };
+  // base url
+  publicPath: process.env.NODE_ENV === 'production'
+      ? './'
+      : '/',
+  // output dir
+  outputDir: './dist',
+  assetsDir: 'static',
+  // eslint-loader check
+  lintOnSave: true,
+  // webpack
+  // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
+  chainWebpack: config => {},
+  // generate map
+  productionSourceMap: true,
+  //use template in vue
+  runtimeCompiler: true,
+  // css
+  css: {
+    // ExtractTextPlugin
+    extract: false,
+    //  CSS source maps?
+    sourceMap: false,
+    // css loader
+    loaderOptions: {
+      postcss: {
+        config: {
+          path: '.postcss.config.js'
+        }
+      }
     },
-};
+    // CSS modules for all css / pre-processor files.
+    requireModuleExtension: true
+  },
+  // use thread-loader for babel & TS in production build
+  // enabled by default if the machine has more than 1 cores
+  parallel: require('os').cpus().length > 1,
+  // webpack-dev-server
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080,
+    before: app => {
+    },
+    proxy: {
+      '/nemflash': {
+        target: 'http://rssmix.com/u/11801188/rss.xml',
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: { '^/nemflash': '' }
+      },
+    }
+  },
+  // plugins
+  pluginOptions: {
+    "process.env": {
+      NODE_ENV: '"development"',
+      PACKAGE_VERSION: packageVersion,
+      WEB: web,
+    }
+  }
+}
