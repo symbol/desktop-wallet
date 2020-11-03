cc7cc12987a5f6fecaba4b8aa0f40677a80efb59
james-lin71 <jameslin848@gmail.com>
2020-11-03 17:40:52 -0300

    add metadata creation form

---
+++ src/views/forms/FormMetadataCreation/FormMetadataCreation.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="update-metadata-modal-conatiner">
+        <div class="modal-title">{{ title }}</div>
+        <form action="processSubmit" onsubmit="event.preventDefault()" class="form-line-container mt-3">
+            <FormRow>
+                <template v-slot:label> {{ $t('form_label_by_account') }}: </template>
+                <template v-slot:inputs>
+                    
+                </template>
+            </FormRow>
+            <button
+                class="button-style fat-button inverted-button left-side-button"
+                type="submit"
+                @click="$emit('on-create')"
+            >
+                {{ $t('confirm') }}
+            </button>
+        </form>
+    </div>
+</template>
+
+<script lang="ts">
+import { FormMetadataCreationTs } from './FormMetadataCreationTs'
+export default class FormMetadataCreation extends FormMetadataCreationTs {}
+</script>
+
+<style lang="less" scoped>
+@import './FormMetadataCreation.less';
+</style>
