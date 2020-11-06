617de5e34c6ea0607787e41da5bebfa96bcdc817
james-lin71 <jameslin848@gmail.com>
2020-11-06 09:21:54 -0300

    update form metadata creation for validate

--- src/views/forms/FormMetadataCreation/FormMetadataCreation.vue
+++ src/views/forms/FormMetadataCreation/FormMetadataCreation.vue
@@ -1,99 +1,126 @@
 <template>
     <div class="update-metadata-modal-conatiner">
-        <div class="modal-title">{{ title }}</div>
-        <form action="processSubmit" onsubmit="event.preventDefault()" class="form-line-container mt-3">
-            <FormRow>
-                <template v-slot:label> {{ $t('form_label_by_account') }}: </template>
-                <template v-slot:inputs>
-                    <div class="select-container">
-                        <Select
-                            v-model="formItems.accountAddress"
-                            :placeholder="$t('select_accounts')"
-                            class="select-size select-style"
-                        >
-                            <Option v-for="(account, index) in knownAccounts" :key="index" :value="account.address">
-                                {{ account.address }}
-                            </Option>
-                        </Select>
-                    </div>
-                </template>
-            </FormRow>
-            <FormRow>
-                <template v-slot:label> {{ $t('form_label_target_mosaic_id') }}: </template>
-                <template v-slot:inputs>
-                    <div class="input-container">
-                        <input
-                            class="input-size input-style"
-                            v-model="formItems.targetAccount"
-                            :placeholder="$t('form_label_target_account_hint')"
-                            type="text"
-                        />
-                    </div>
-                </template>
-            </FormRow>
-            <FormRow>
-                <template v-slot:label> {{ $t('form_label_scoped_metadata_key') }}: </template>
-                <template v-slot:inputs>
-                    <div class="select-container">
-                        <Select
+        <div class="modal-title">{{ $t(modalTitle) }}</div>
+        <ValidationObserver v-slot="{ handleSubmit }" ref="observer" slim>
+            <form autocomplete="off" onsubmit="event.preventDefault()" class="form-line-container mt-3">
+                <FormRow>
+                    <template v-slot:label> {{ $t('form_label_by_account') }}: </template>
+                    <template v-slot:inputs>
+                        <div class="select-container">
+                            <Select
+                                v-model="formItems.accountAddress"
+                                :placeholder="$t('select_accounts')"
+                                class="select-size select-style"
+                            >
+                                <Option v-for="(account, index) in knownAccounts" :key="index" :value="account.address">
+                                    {{ account.address }}
+                                </Option>
+                            </Select>
+                        </div>
+                    </template>
+                </FormRow>
+                <FormRow>
+                    <template v-slot:label> {{ $t('form_label_target_account') }}: </template>
+                    <template v-slot:inputs>
+                        <div class="input-container">
+                            <input
+                                class="input-size input-style"
+                                autocomplete="false"
+                                v-model="formItems.targetAccount"
+                                :placeholder="$t('form_label_target_account_hint')"
+                                type="text"
+                            />
+                        </div>
+                    </template>
+                </FormRow>
+                <FormRow v-if="type !== 0">
+                    <template v-slot:label> {{ $t(targetLabel) }}: </template>
+                    <template v-slot:inputs>
+                        <div class="select-container">
+                            <Select
+                                v-model="formItems.targetId"
+                                :placeholder="$t('select')"
+                                class="select-size select-style"
+                            >
+                                <Option v-for="(targetId, index) in ownedTargetHexIds" :key="index" :value="targetId">
+                                    {{ targetId }}
+                                </Option>
+                            </Select>
+                        </div>
+                    </template>
+                </FormRow>
+                <FormRow>
+                    <template v-slot:label> {{ $t('form_label_scoped_metadata_key') }}: </template>
+                    <template v-slot:inputs>
+                        <AutoComplete
+                            class="auto-complete-size auto-complete-style"
                             v-model="formItems.scopedKey"
                             :placeholder="$t('form_label_scoped_metadata_key_hint')"
-                            class="select-size select-style"
-                        >
-                        </Select>
-                    </div>
-                </template>
-            </FormRow>
-            <FormRow>
-                <template v-slot:label> {{ $t('form_label_value') }}: </template>
-                <template v-slot:inputs>
-                    <div class="value-container">
-                        <textarea
-                            class="metadata-value-input"
-                            :placeholder="$t('form_label_value_hint')"
-                            v-model="formItems.value" 
+                            :data="cashedScopedKeys"
                         />
-                    </div>
-                </template>
-            </FormRow>
-            <FormRow>
-                <template v-slot:label> {{ $t('form_label_max_fee') }}: </template>
-                <template v-slot:inputs>
-                    <MaxFeeSelector v-model="formItems.maxFee" />
-                </template>
-            </FormRow>
-            <FormRow>
-                <template v-slot:label> {{ $t('form_label_password') }}: </template>
-                <template v-slot:inputs>
-                    <ValidationProvider
-                        v-slot="{ errors }"
-                        vid="password"
-                        mode="lazy"
-                        :name="$t('password')"
-                        :rules="validationRules.password"
-                        tag="div"
-                        class="select-container"
-                    >
-                        <ErrorTooltip :errors="errors">
-                            <input
-                                ref="passwordInput"
-                                v-model="formItems.password"
-                                class="input-size input-style"
-                                :placeholder="$t('form_label_password_hint')"
-                                type="password"
-                            />
-                        </ErrorTooltip>
-                        <button
-                            class="button-style fat-button inverted-button right-side-button"
-                            type="submit"
-                            @click="$emit('on-create')"
+                    </template>
+                </FormRow>
+                <FormRow>
+                    <template v-slot:label> {{ $t('form_label_value') }}: </template>
+                    <template v-slot:inputs>
+                        <ValidationProvider
+                            v-slot="{ errors }"
+                            vid="value"
+                            mode="lazy"
+                            :name="$t('form_label_value')"
+                            :rules="`required`"
+                            tag="div"
+                            class="row-metadata-input value-container"
                         >
-                            {{ $t('confirm') }}
-                        </button>
-                    </ValidationProvider>
-                </template>
-            </FormRow>
-        </form>
+                            <ErrorTooltip :errors="errors">
+                                <textarea
+                                    class="metadata-value-input"
+                                    :placeholder="$t('form_label_value_hint')"
+                                    v-model="formItems.value" 
+                                />
+                            </ErrorTooltip>
+                        </ValidationProvider>
+                    </template>
+                </FormRow>
+                <FormRow>
+                    <template v-slot:label> {{ $t('form_label_max_fee') }}: </template>
+                    <template v-slot:inputs>
+                        <MaxFeeSelector v-model="formItems.maxFee" />
+                    </template>
+                </FormRow>
+                <FormRow>
+                    <template v-slot:label> {{ $t('form_label_password') }}: </template>
+                    <template v-slot:inputs>
+                        <ValidationProvider
+                            v-slot="{ errors }"
+                            vid="password"
+                            mode="lazy"
+                            :name="$t('password')"
+                            :rules="validationRules.profilePassword"
+                            tag="div"
+                            class="select-container"
+                        >
+                            <ErrorTooltip :errors="errors">
+                                <input
+                                    v-model="formItems.password"
+                                    class="input-size input-style"
+                                    :placeholder="$t('form_label_password_hint')"
+                                    autocomplete="new-password"
+                                    type="password"
+                                />
+                            </ErrorTooltip>
+                            <button
+                                class="button-style fat-button inverted-button right-side-button"
+                                type="submit"
+                                @click="handleSubmit(onSubmit)"
+                            >
+                                {{ $t('confirm') }}
+                            </button>
+                        </ValidationProvider>
+                    </template>
+                </FormRow>
+            </form>
+        </ValidationObserver>
     </div>
 </template>
 
--- src/views/forms/FormMetadataCreation/FormMetadataCreationTs.ts
+++ src/views/forms/FormMetadataCreation/FormMetadataCreationTs.ts
@@ -14,11 +14,16 @@
  *
  */
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import { MetadataType } from 'symbol-sdk';
 import { mapGetters } from 'vuex';
 
+// @ts-ignore
 import { ValidationRuleset } from '@/core/validation/ValidationRuleset';
-import { NotificationType } from '@/core/utils/NotificationType';
-import { MetadataService } from '@/services/MetadataService';
+
+import { ScopedMetadataKeysHelpers } from '@/core/utils/ScopedMetadataKeysHelpers';
+import { NamespaceModel } from '@/core/database/entities/NamespaceModel';
+import { MosaicModel } from '@/core/database/entities/MosaicModel';
+import { AccountModel } from '@/core/database/entities/AccountModel';
 
 // child components
 import { ValidationObserver, ValidationProvider } from 'vee-validate';
@@ -39,19 +44,24 @@ import ErrorTooltip from '@/components/ErrorTooltip/ErrorTooltip.vue';
     },
     computed: {
         ...mapGetters({
-            knownAccounts: 'account/knownAccounts',
+            currentAccount: 'account/currentAccount',
+            knownAccounts: 'account/knownAccounts',            namespaces: 'namespace/ownedNamespaces',
+            ownedMosaics: 'mosaic/ownedMosaics',
+            ownedNamespaces: 'namespace/ownedNamespaces',
         })
     },
-    mounted: function() {
-        console.log("----------", this.knownAccounts);
-    }
 })
 export class FormMetadataCreationTs extends Vue {
 
+    /**
+     * Metadata type
+     * @type {MetadataType}
+     */
     @Prop({
-        default: ''
+        default: MetadataType.Account,
+        required: true,
     })
-    protected title: string;
+    protected type: MetadataType;
 
     /**
      * Form fields
@@ -60,10 +70,125 @@ export class FormMetadataCreationTs extends Vue {
     public formItems = {
         accountAddress: '',
         targetAccount: '',
+        targetId: '',
         scopedKey: '',
         metadataValue: '',
         maxFee: '',
         password: ''
+    }
+    
+    /**
+     * Known accounts
+     * @protected
+     * @var {AccountModel[]}
+     */
+    protected knownAccounts: AccountModel[];
+
+    /**
+     * Current account owned mosaics
+     * @protected
+     * @type {MosaicModel[]}
+     */
+    protected ownedMosaics: MosaicModel[];
+
+    /**
+     * Current account owned namespaces
+     * @protected
+     * @type {NamespaceModel[]}
+     */
+    protected ownedNamespaces: NamespaceModel[];
+
+    /**
+     * Current account
+     * @protected
+     * @type {AccountModel}
+     */
+    protected currentAccount: AccountModel;
+
+    /**
+     * Mosaic check
+     * @return {boolean}
+     */
+    protected isMosaic(): boolean {
+        return this.type === MetadataType.Mosaic;
+    }
+
+    /**
+     * Reset the form with properties
+     * @return {void}
+     */
+    protected resetForm() {
+        this.formItems.accountAddress = this.currentAccount.address;
+        this.formItems.metadataValue = '';
+        this.formItems.scopedKey = '';
+        this.formItems.metadataValue = '';
+    }
+
+    /**
+     * Modal title from modal type
+     * @type {string}
+     */
+    get modalTitle(): string {
+        let title : string = '';
+        switch(this.type) {
+            case MetadataType.Mosaic:
+                title = 'modal_title_mosaic_metadata';
+                break;
+            
+            case MetadataType.Namespace:
+                title = 'modal_title_namespace_metadata';
+                break;
+
+            default:
+                title = 'modal_title_account_metadata';
+                break;
+        }
+        return title;
+    }
+
+    /**
+     * Target account or public key label depends on MetadataType
+     * @param {void}
+     * @returns {string}
+     */
+    get targetLabel() : string {
+        let title : string = '';
+        switch(this.type) {
+            case MetadataType.Mosaic:
+                title = 'form_label_target_mosaic_id';
+                break;
+            
+            case MetadataType.Namespace:
+                title = 'form_label_target_namespace_id';
+                break;
+        }
+        return title;
+    }
+    
+    /**
+     * Default explorer link list
+     * @readonly
+     * @type {string[]}
+     */
+    get cashedScopedKeys() {
+        return ScopedMetadataKeysHelpers.loadScopedMetadataKeys();
+    }
+
+    /**
+     * Target id validator name
+     * @return {string}
+     */
+    get targetIdValidatorName(): string {
+        return this.isMosaic() ? 'mosaic_id' : 'namespace_id';
+    }
+
+    get ownedTargetHexIds() : string[] {
+        return this.type === MetadataType.Namespace
+            ? this.ownedNamespaces.map(({ namespaceIdHex }) => namespaceIdHex)
+            : this.ownedMosaics
+                .filter(({ ownerRawPlain }) => ownerRawPlain === this.currentAccount.address)
+                .map(({ mosaicIdHex }) => mosaicIdHex);
+
     }
 
     /**
@@ -76,8 +201,19 @@ export class FormMetadataCreationTs extends Vue {
      * Persist created metadata
      * @return {void}
      */
-    private persistMetadata() {
+    protected onSubmit() : void {
+        ScopedMetadataKeysHelpers.storeKey(this.formItems.scopedKey);
 
+        switch(this.type) {
+            case MetadataType.Account:
+                break;
+
+            case MetadataType.Mosaic:
+                break;
+
+            case MetadataType.Namespace:
+                break;
+        }
     }
 
 }
