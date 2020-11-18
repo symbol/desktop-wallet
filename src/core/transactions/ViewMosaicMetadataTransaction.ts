3ead1a29a9f7571f5ddb6c236b5a9d4f6d5841f3
Bassem <bassem.magdyy94@gmail.com>
2020-11-18 03:25:02 +0200

    linting

--- src/core/transactions/ViewMosaicMetadataTransaction.ts
+++ src/core/transactions/ViewMosaicMetadataTransaction.ts
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright 2020 Grégory Saive for NEM (https://nem.io)
+ * Copyright 2020 for NEM (https://nem.io)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
