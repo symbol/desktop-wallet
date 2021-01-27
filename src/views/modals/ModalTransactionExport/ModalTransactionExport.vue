b103b53cd49a275a44f2710c967f3b422beb97ae
myxmaster <77545287+myxmaster@users.noreply.github.com>
2021-01-27 22:05:22 +0100

    removed unnecessary whitespace, fixed download button alignment (#920)

--- src/views/modals/ModalTransactionExport/ModalTransactionExport.vue
+++ src/views/modals/ModalTransactionExport/ModalTransactionExport.vue
@@ -9,15 +9,17 @@
                     <span class="subtitle">{{ $t('accounts_backup_transactions') }}</span>
                     <p>{{ $t('accounts_backup_transactions_description') }}</p>
                 </div>
-                <button class="button-style validation-button download-button" type="button" @click="onDownloadTx">
+            </div>
+            <div slot="footer" class="modal-footer">
+                <button
+                    v-if="hasTransactionExportInfo"
+                    class="button-style inverted-button download-button"
+                    type="button"
+                    @click="onDownloadTx"
+                >
                     <Icon :type="'md-download'" size="20" />
                     <span>&nbsp;{{ $t('button_download_qr') }}</span>
                 </button>
-            </div>
-            <div slot="footer" class="modal-footer">
-                <!--<button type="submit" class="centered-button button-style back-button float-right" @click="show = false">
-                    {{ $t('close') }}
-                </button>-->
             </div>
         </Modal>
     </div>
