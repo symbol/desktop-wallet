784d262c6837a98859517020f4060fefa7dbdea2
Bassem <bassem.magdyy94@gmail.com>
2021-02-11 11:26:29 +0200

    add terms and conditions to ledger integration (#1095)

--- src/views/pages/profiles/access-ledger/finalize/Finalize.vue
+++ src/views/pages/profiles/access-ledger/finalize/Finalize.vue
@@ -2,12 +2,33 @@
     <div class="finish-sec scroll" @keyup.enter="submit">
         <div class="finish-col">
             <div class="finish-left">
+                <p class="text1">
+                    {{ $t('terms_and_conditions') }}
+                </p>
+                <div class="flex bottom-space term">
+                    <Checkbox v-model="marked">
+                        <span class="link">{{ $t('please_approve') }}</span>
+                        <a class="link" href="#/terms">
+                            {{ $t('terms_and_conditions') }}
+                        </a>
+                        &
+                        <a class="link" href="#/privacy">
+                            {{ $t('privacy_policy') }}
+                        </a>
+                        <span>.</span>
+                    </Checkbox>
+                </div>
                 <div class="buttons clear">
                     <div class="float-right">
                         <button type="button" class="solid-button button-style fat-button create-account-style" @click="$router.back()">
                             {{ $t('back') }}
                         </button>
-                        <button type="submit" class="inverted-button button-style fat-button create-account-style" @click="submit">
+                        <button
+                            type="submit"
+                            class="inverted-button button-style fat-button create-account-style"
+                            :disabled="!marked"
+                            @click="submit"
+                        >
                             {{ $t('next') }}
                         </button>
                     </div>
