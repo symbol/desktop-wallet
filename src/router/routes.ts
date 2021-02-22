938fbacc0f7f74c98d7f1f714bd5990c53e51679
myxmaster <sebi.mueller.83@gmail.com>
2021-02-22 20:07:10 +0100

    fixed navigation tabs icons

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -56,7 +56,7 @@ export const routes: AppRoute[] = [
                         meta: {
                             protected: true,
                             title: 'page_title_send',
-                            icon: officialIcons.incoming,
+                            icon: officialIcons.outgoing,
                         },
                         // @ts-ignore
                         component: () => import('@/views/pages/dashboard/transfer/DashboardTransferPage.vue'),
@@ -68,7 +68,7 @@ export const routes: AppRoute[] = [
                         meta: {
                             protected: true,
                             title: 'page_title_invoice',
-                            icon: officialIcons.outgoing,
+                            icon: officialIcons.incoming,
                         },
                         // @ts-ignore
                         component: () => import('@/views/pages/dashboard/invoice/DashboardInvoicePage.vue'),
