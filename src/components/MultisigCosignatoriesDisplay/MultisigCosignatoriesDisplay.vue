f36db6b0341cc5d96d3d5299c5e2935150f471a3
Bassem <bassem.magdyy94@gmail.com>
2021-06-10 18:13:21 +0200

    fix button style

--- src/components/MultisigCosignatoriesDisplay/MultisigCosignatoriesDisplay.vue
+++ src/components/MultisigCosignatoriesDisplay/MultisigCosignatoriesDisplay.vue
@@ -96,7 +96,7 @@
 
             <AddCosignatoryInput v-if="isAddingCosignatory" @added="onAddCosignatory" />
             <div v-if="!isAddingCosignatory" class="row-cosignatory-modification-display inputs-container link mx-1">
-                <ButtonAdd class="icon-left-button clickable" :size="24" @click="isAddingCosignatory = true" />
+                <ButtonAdd class="clickable" :size="24" @click="isAddingCosignatory = true" />
                 <a href="#" style="color: #5200c6;" @click="isAddingCosignatory = true">{{ $t('form_label_add_cosignatory') }}</a>
             </div>
         </div>
