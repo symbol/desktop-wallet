f6fe4d7f3b3c5802e338e84f07030d9758454f89
AnthonyLaw <yc-law1015@hotmail.com>
2021-06-18 21:21:55 +0800

    fix: adjust css for tooltips and lint

--- src/components/TransactionList/TransactionRow/TransactionRow.vue
+++ src/components/TransactionList/TransactionRow/TransactionRow.vue
@@ -28,11 +28,11 @@
                         <div slot="content">
                             <!-- allow top 5 mosaics show in the tooltip -->
                             <div v-for="mosaic in nonNativeMosaicList().slice(0, numberOfShowMosicsTooltips)" :key="mosaic.id">
-                                {{mosaic.name}} - {{mosaic.relativeAmount}}
+                                {{ mosaic.name }} - {{ mosaic.relativeAmount }}
                             </div>
 
                             <div v-if="nonNativeMosaicList().length - numberOfShowMosicsTooltips > 0">
-                                {{ $t('tooltip_mosaic_view_more', {count: nonNativeMosaicList().length - numberOfShowMosicsTooltips}) }}
+                                {{ $t('tooltip_mosaic_view_more', { count: nonNativeMosaicList().length - numberOfShowMosicsTooltips }) }}
                             </div>
                         </div>
                     </Tooltip>
