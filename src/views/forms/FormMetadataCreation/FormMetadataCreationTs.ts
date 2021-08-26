a19395108cbc78218314666235f61f315171071a
Baha <bahadir.yilmaz@nem.software>
2021-08-26 09:23:41 +0100

    fix: avoid unnecessary agg bonded and use complete instead

--- src/views/forms/FormMetadataCreation/FormMetadataCreationTs.ts
+++ src/views/forms/FormMetadataCreation/FormMetadataCreationTs.ts
@@ -212,6 +212,9 @@ export class FormMetadataCreationTs extends FormTransactionBase {
     protected getTransactionCommandMode(): TransactionCommandMode {
         const target = this.getTargetAddress().plain();
         if (this.selectedSigner.multisig) {
+            if (this.requiredCosignatures === 1 && this.formItems.signerAddress === target) {
+                return TransactionCommandMode.AGGREGATE;
+            }
             return TransactionCommandMode.MULTISIGN;
         } else if (this.formItems.signerAddress === target) {
             return TransactionCommandMode.AGGREGATE;
