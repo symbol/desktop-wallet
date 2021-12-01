3e3f46f184e4700e52fd4b5520b91dcdd6e3f510
Bassem <bassem.magdyy94@gmail.com>
2021-12-01 16:37:54 +0200

    - language selector not fully visible because update warning #1798 (#1800)
    fix copyrights for korean language
    
    remove nem.io urls
    
    rename mocked accounts for better readability

--- __mocks__/Accounts.ts
+++ __mocks__/Accounts.ts
@@ -1,5 +1,5 @@
 /*
- * (C) Symbol Contributors 2021 (https://nem.io)
+ * (C) Symbol Contributors 2021
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -70,7 +70,7 @@ const TEST_ACCOUNTS = {
         networkType: NetworkType.TEST_NET,
         privateKey: '803040D4A33983C4B233C6C2054A24B9C655E8CAC6C06AECCED56B8FE424FF2B',
     },
-    remoteTestnet2: {
+    remoteTestnetRecipient: {
         networkType: NetworkType.TEST_NET,
         privateKey: '893040D4A33983C4B233C6C2054A24B9C655E8CAC6C06AECCED56B8FE424FF2B',
     },
--- __tests__/database/storage/ProfileModelStorage.spec.ts
+++ __tests__/database/storage/ProfileModelStorage.spec.ts
@@ -1,5 +1,5 @@
 /*
- * (C) Symbol Contributors 2021 Foundation (https://nem.io)
+ * (C) Symbol Contributors 2021 Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
