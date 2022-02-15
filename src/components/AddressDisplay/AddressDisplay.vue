27612eb13522e0786f6949b5a932da4e8da641af
Oleg Makarenko <olegomm@gmail.com>
2022-02-15 15:24:34 +0200

    feat: add address popover with explorer url

--- src/components/AddressDisplay/AddressDisplay.vue
+++ src/components/AddressDisplay/AddressDisplay.vue
@@ -1,10 +1,20 @@
 <template>
-    <a v-if="allowExplorerLink" target="_blank" :href="explorerUrl">{{ descriptor }}</a>
+    <Poptip v-if="allowExplorerLink" trigger="hover">
+        {{ descriptor }}
+        <div slot="content">
+            <div slot="title" class="address">
+                {{ $t('address') }}: <span class="address-value"> {{ rawAddress }} </span>
+            </div>
+            <a class="link" target="_blank" :href="explorerUrl">{{ $t('accounts_view_explorer_description') }}</a>
+        </div>
+    </Poptip>
+    
     <span v-else>{{ descriptor }}</span>
 </template>
 
 <script lang="ts">
 import { AddressDisplayTs } from './AddressDisplayTs';
+import './AddressDisplay.less';
 
 export default class AddressDisplay extends AddressDisplayTs {}
 </script>
