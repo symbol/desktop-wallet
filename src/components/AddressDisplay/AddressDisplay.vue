04f9ed9477cadba3e0478ccff025b886e5a7fcbc
Oleg Makarenko <olegomm@gmail.com>
2022-05-11 18:29:26 +0300

    task: update translations, add locale unit test

--- src/components/AddressDisplay/AddressDisplay.vue
+++ src/components/AddressDisplay/AddressDisplay.vue
@@ -1,16 +1,16 @@
 <template>
     <a v-if="allowExplorerLink && descriptor === rawAddress" class="address-display link" target="_blank" :href="explorerUrl">
-        <span v-if="isAddressBlocked" class="blocked-tag">{{ $t('blacklist_address_label') }}</span>
+        <span v-if="isAddressBlocked" class="blocked-tag">{{ $t('blocked_address_label') }}</span>
         <span>{{ descriptor }}</span>
     </a>
     <Tooltip v-else-if="allowExplorerLink" :content="rawAddress" placement="right">
         <a class="address-display link" target="_blank" :href="explorerUrl">
-            <span v-if="isAddressBlocked" class="blocked-tag">{{ $t('blacklist_address_label') }}</span>
+            <span v-if="isAddressBlocked" class="blocked-tag">{{ $t('blocked_address_label') }}</span>
             <span>{{ descriptor }}</span>
         </a>
     </Tooltip>
     <span v-else class="address-display">
-        <span v-if="isAddressBlocked" class="blocked-tag">{{ $t('blacklist_address_label') }}</span>
+        <span v-if="isAddressBlocked" class="blocked-tag">{{ $t('blocked_address_label') }}</span>
         <span>{{ descriptor }}</span>
     </span>
 </template>
