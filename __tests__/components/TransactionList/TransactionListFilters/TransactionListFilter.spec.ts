ab49a47090508353d44901e4b4b08bee2c0d76b3
AnthonyLaw <anthony@symbol.dev>
2022-07-14 16:59:56 +0800

    [wallet/desktop] task: refactor on TransactionListFilters

--- __tests__/components/TransactionList/TransactionListFilters/TransactionListFilter.spec.ts
+++ __tests__/components/TransactionList/TransactionListFilters/TransactionListFilter.spec.ts
@@ -1,50 +1,77 @@
 import { Address } from 'symbol-sdk';
-//@ts-ignore
 import TransactionListFilters from '@/components/TransactionList/TransactionListFilters/TransactionListFilters.vue';
+import { TransactionListFiltersTs } from '@/components/TransactionList/TransactionListFilters/TransactionListFiltersTs';
 import { getComponent } from '@MOCKS/Components';
 import AccountStore from '@/store/Account';
 import TransactionStore from '@/store/Transaction';
 import AddressBookStore from '@/store/AddressBook';
 import { getTestAccount } from '@MOCKS/Accounts';
-let wrapper;
-let vm;
 import { addressBookMock } from '@MOCKS/AddressBookMock';
-const currentSigner = getTestAccount('remoteTestnet');
-const isBlackListFilterActivated = false;
 
-beforeEach(() => {
-    wrapper = getComponent(
-        TransactionListFilters,
-        { account: AccountStore, transaction: TransactionStore, addressBook: AddressBookStore },
-        {
-            currentAccount: null,
-            currentAccountSigner: currentSigner,
-            addressBook: addressBookMock,
-            isBlackListFilterActivated: isBlackListFilterActivated,
-            signers: [],
-        },
-        {},
-        {},
-    );
-    vm = wrapper.vm as TransactionListFilters;
-});
-afterEach(() => {
-    wrapper.destroy();
-});
-const addr = Address.createFromRawAddress('TAD5BAHLOIXCRRB6GU2H72HPXMBBVAEUQRYPHBY');
-describe('TransactionListFilters', () => {
-    test("should call the 'account/SET_CURRENT_SIGNER' with address", () => {
-        vm.onSignerSelectorChange('TAD5BAHLOIXCRRB6GU2H72HPXMBBVAEUQRYPHBY');
-        expect(vm.$store.dispatch).toBeCalledWith('account/SET_CURRENT_SIGNER', { address: addr, reset: true, unsubscribeWS: false });
+describe('TransactionList/TransactionListFilters', () => {
+    const currentSigner = getTestAccount('remoteTestnet');
+
+    const getTransactionListFilterWrapper = (isBlackListFilterActivated: boolean) => {
+        return getComponent(
+            TransactionListFilters,
+            { account: AccountStore, transaction: TransactionStore, addressBook: AddressBookStore },
+            {
+                currentAccount: null,
+                currentAccountSigner: currentSigner,
+                addressBook: addressBookMock,
+                isBlackListFilterActivated: isBlackListFilterActivated,
+                signers: [],
+            },
+            {},
+            {
+                Tooltip: true,
+                'font-awesome-icon': true,
+            },
+        );
+    };
+
+    test("store dispatch 'account/SET_CURRENT_SIGNER' when provided address", () => {
+        // Arrange:
+        const address = 'TAD5BAHLOIXCRRB6GU2H72HPXMBBVAEUQRYPHBY';
+
+        const wrapper = getTransactionListFilterWrapper(false);
+        const vm = wrapper.vm as TransactionListFiltersTs;
+
+        // Act:
+        // @ts-ignore
+        vm.onSignerSelectorChange(address);
+
+        // Assert:
+        expect(vm.$store.dispatch).toBeCalledWith('account/SET_CURRENT_SIGNER', {
+            address: Address.createFromRawAddress(address),
+            reset: true,
+            unsubscribeWS: false,
+        });
     });
 
-    test("should not call the 'account/SET_CURRENT_SIGNER' without address", () => {
+    test("store dispatch ignore 'account/SET_CURRENT_SIGNER' when address not provided", () => {
+        // Arrange:
+        const wrapper = getTransactionListFilterWrapper(false);
+        const vm = wrapper.vm as TransactionListFiltersTs;
+
+        // Act:
+        // @ts-ignore
         vm.onSignerSelectorChange();
+
+        // Assert:
         expect(vm.$store.dispatch).not.toBeCalled();
     });
 
-    test("should call 'transaction/filterTransactions' with blacklisted contacts", () => {
+    test("store commit 'transaction/filterTransactions' with blacklisted contacts", () => {
+        // Arrange:
+        const wrapper = getTransactionListFilterWrapper(false);
+        const vm = wrapper.vm as TransactionListFiltersTs;
+
+        // Act:
+        // @ts-ignore
         vm.onSelectBlackListed();
+
+        // Assert:
         expect(vm.$store.commit).toBeCalledWith('transaction/filterTransactions', {
             filterOption: null,
             currentSignerAddress: currentSigner.address.plain(),
@@ -52,29 +79,36 @@ describe('TransactionListFilters', () => {
             shouldFilterOptionChange: false,
             blacklistedContacts: addressBookMock.getBlackListedContacts(),
         });
-        expect(vm.$store.commit).toBeCalledWith('transaction/isBlackListFilterActivated', !isBlackListFilterActivated);
+        expect(vm.$store.commit).toBeCalledWith('transaction/isBlackListFilterActivated', true);
     });
 
-    test("should call 'transaction/filterTransactions'", () => {
-        wrapper = getComponent(
-            TransactionListFilters,
-            { account: AccountStore, transaction: TransactionStore, addressBook: AddressBookStore },
-            {
-                currentAccount: null,
-                currentAccountSigner: currentSigner,
-                addressBook: addressBookMock,
-                isBlackListFilterActivated: true,
-                signers: [],
-            },
-            {},
-            {},
-        );
-        vm = wrapper.vm as TransactionListFilters;
+    test("store commit 'transaction/filterTransactions'", () => {
+        // Arrange:
+        const wrapper = getTransactionListFilterWrapper(true);
+        const vm = wrapper.vm as TransactionListFiltersTs;
+
+        // Act:
+        // @ts-ignore
         vm.onSelectBlackListed();
+
+        // Assert:
         expect(vm.$store.commit).toBeCalledWith('transaction/filterTransactions', {
             currentSignerAddress: currentSigner.address.plain(),
             filterOption: null,
         });
         expect(vm.$store.commit).toBeCalledWith('transaction/isBlackListFilterActivated', false);
+    });
+
+    test('returns emit downloadTransactions', () => {
+        // Arrange:
+        const wrapper = getTransactionListFilterWrapper(false);
+        const vm = wrapper.vm as TransactionListFiltersTs;
+
+        // Act:
+        vm.downloadTransactions();
+
+        // Assert:
+        expect(wrapper.emitted('downloadTransactions')).toBeTruthy();
+        expect(wrapper.emitted('downloadTransactions')).toEqual([[]]);
     });
 });
