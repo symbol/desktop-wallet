7239656dd02a569f4416b96bc7d1a0f43584a31f
AnthonyLaw <anthony@symbol.dev>
2022-07-17 02:42:42 +0800

    [wallet/desktop] task: added unit test for TransactionListHeaderFilter

---
+++ __tests__/components/TransactionList/TransactionListHeader/TransactionListHeader.spec.ts
@@ -0,0 +1,17 @@
+//@ts-ignore
+import TransactionListHeader from '@/components/TransactionList/TransactionListHeader/TransactionListHeader.vue';
+import { getComponent } from '@MOCKS/Components';
+
+describe('TransactionListHeaderFilter', () => {
+    test('render transaction list table header', () => {
+        // Arrange:
+        const wrapper = getComponent(TransactionListHeader, {}, {}, {}, {});
+
+        // Act + Assert:
+        expect(wrapper.findAll('.transaction-table-columns span').length).toBe(5);
+        expect(wrapper.find('.icon-header').exists()).toBe(true);
+        expect(wrapper.find('.amount-header').exists()).toBe(true);
+        expect(wrapper.find('.confirmation-header').exists()).toBe(true);
+        expect(wrapper.find('.hash-header').exists()).toBe(true);
+    });
+});
