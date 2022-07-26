126dcc0ed015f2de543e8e9103c845028ee5d18b
Baha <baha@symbol.dev>
2022-07-26 13:27:00 +0300

    [wallet/desktop] fix: jest non-js assets transformer added
    problem: jest-transform-stub was stubbing all of the non-js assets imported,img refs were empty
             this was causing image assertions to fail.
    solution: transform assets so that their file names are exported in jest environment

--- jest.config.js
+++ jest.config.js
@@ -1,52 +1,38 @@
-const path = require('path')
+const path = require('path');
 module.exports = {
-  rootDir: path.join(__dirname),
-  moduleFileExtensions: [
-    'js',
-    'jsx',
-    'json',
-    'vue',
-    'ts',
-    'tsx',
-    'node'
-  ],
-  moduleDirectories: ['node_modules', 'src'],
-  modulePaths: ['<rootDir>/src', '<rootDir>/node_modules'],
-  transform: {
-    "^.+\\.js$": "babel-jest",
-    '^.+\\.vue$': 'vue-jest',
-    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2|ogg)$': 'jest-transform-stub',
-    "^.+\\.ts$": "ts-jest",
-  },
-  transformIgnorePatterns: [
-    '<rootDir>/node_modules/?!(vee-validate/dist/rules)',
-  ],
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
-    "^@MOCKS/(.*)$": "<rootDir>/__mocks__/$1",
-    "\\.(css|less)$": "identity-obj-proxy"
-  },
-  snapshotSerializers: [
-    'jest-serializer-vue'
-  ],
-  testMatch: [
-    '**/__tests__/**/**/*.spec.ts',
-  ],
-  testURL: 'http://localhost/',
-  globals: {
-    'ts-jest': {
-      babelConfig: true,
+    rootDir: path.join(__dirname),
+    moduleFileExtensions: ['js', 'jsx', 'json', 'vue', 'ts', 'tsx', 'node'],
+    moduleDirectories: ['node_modules', 'src'],
+    modulePaths: ['<rootDir>/src', '<rootDir>/node_modules'],
+    transform: {
+        '^.+\\.js$': 'babel-jest',
+        '^.+\\.vue$': 'vue-jest',
+        '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2|ogg)$': './jestFileTransformer.js',
+        '^.+\\.ts$': 'ts-jest',
     },
-  },
-  coverageDirectory: "coverage",
-  collectCoverageFrom: [
-    "**/*.ts",
-    "**/*.vue",
-    "!**/node_modules/**",
-    "!**/__mocks__/**",
-    "!**/*.png",
-    "!**/*.ogg",
-    "!**/*.d.ts",
-    "!**/*.less",
-  ],
-}
+    transformIgnorePatterns: ['<rootDir>/node_modules/?!(vee-validate/dist/rules)'],
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+        '^@MOCKS/(.*)$': '<rootDir>/__mocks__/$1',
+        '\\.(css|less)$': 'identity-obj-proxy',
+    },
+    snapshotSerializers: ['jest-serializer-vue'],
+    testMatch: ['**/__tests__/**/**/*.spec.ts'],
+    testURL: 'http://localhost/',
+    globals: {
+        'ts-jest': {
+            babelConfig: true,
+        },
+    },
+    coverageDirectory: 'coverage',
+    collectCoverageFrom: [
+        '**/*.ts',
+        '**/*.vue',
+        '!**/node_modules/**',
+        '!**/__mocks__/**',
+        '!**/*.png',
+        '!**/*.ogg',
+        '!**/*.d.ts',
+        '!**/*.less',
+    ],
+};
