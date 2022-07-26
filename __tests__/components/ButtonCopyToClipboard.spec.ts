56e8ac3f423c82a3e08f4897397c54cec2aa32d6
Baha <baha@symbol.dev>
2022-07-26 13:29:51 +0300

    [wallet/desktop] task: button copy to clipboard test is added

--- __tests__/components/ButtonCopyToClipboard.spec.ts
+++ __tests__/components/ButtonCopyToClipboard.spec.ts
@@ -5,8 +5,10 @@ import ButtonCopyToClipboard from '@/components/ButtonCopyToClipboard/ButtonCopy
 import { ButtonCopyToClipboardTs } from '@/components/ButtonCopyToClipboard/ButtonCopyToClipboardTs';
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import { UIHelpers } from '@/core/utils/UIHelpers';
-const localVue = createLocalVue();
+import { NotificationType } from '@/core/utils/NotificationType';
 
+// Arrange:
+const localVue = createLocalVue();
 const options = {
     localVue,
     Vuex,
@@ -23,19 +25,46 @@ const options = {
 const wrapper = shallowMount(ButtonCopyToClipboard, options);
 
 const vm = wrapper.vm as ButtonCopyToClipboardTs;
-describe('ButtonCopyToClipboard', () => {
+describe('components/ButtonCopyToClipboard', () => {
     test('receive a property "value" correctly', () => {
+        // Assert:
         expect(vm.value).toBe('123');
     });
+
     test('Click on Button should call method "copyToClipboard" when prop "value" exists', () => {
+        // Arrange:
         UIHelpers.copyToClipboard = jest.fn();
+
+        // Act:
         wrapper.find('Button').trigger('click');
+
+        // Assert:
         expect(UIHelpers.copyToClipboard).toBeCalledWith('123');
     });
-    test('Click on Button should call method "copyToClipboard" when prop "value" does not exists', () => {
+
+    test('Click on Button should throw an error on copy failure', () => {
+        // Arrange:
+        UIHelpers.copyToClipboard = jest.fn().mockImplementation(() => {
+            throw new Error('Error');
+        });
+
+        // Act:
+        wrapper.find('Button').trigger('click');
+
+        // Assert:
+        expect(wrapper.vm.$store.dispatch).toBeCalledWith('notification/ADD_ERROR', NotificationType.COPY_FAILED);
+    });
+
+    test('Click on Button should not call method "copyToClipboard" when prop "value" does not exists', async () => {
+        // Arrange:
         UIHelpers.copyToClipboard = jest.fn();
         wrapper.setProps({ value: null });
+        await vm.$nextTick();
+
+        // Act:
         wrapper.find('Button').trigger('click');
-        expect(UIHelpers.copyToClipboard).toBeCalled();
+
+        // Assert:
+        expect(UIHelpers.copyToClipboard).not.toBeCalled();
     });
 });
