fe84d2be3dac3be0e9295dae66b623c74357bf1d
Baha <baha@symbol.dev>
2022-08-19 14:52:42 +0300

    [wallet/desktop] task: release v1.0.11 #1897
    keys-whitelist.json is added to the repo
    notarization scripts are added
    readme updated

--- README.md
+++ README.md
@@ -49,6 +49,44 @@ npm run dev
 
 4. Visit http://localhost:8080/#/ in your browser.
 
+## Release
+1. For local use, build Symbol Wallet Electron app (only Electron version support Ledger wallets), default build for MacOS, Windows and Linux
+
+<pre>
+# to skip code signing
+export CSC_IDENTITY_AUTO_DISCOVERY=false
+
+npm run release
+</pre>
+
+2. Release for distribution: (Code signing for Apple builds - requires `Developer ID Certificate`)
+
+    2.1 On a MacOS machine, download the zip file containing the app signing certificates (ask team)
+
+    2.2 Extract the certificates and double click each one of them to add to the keychain (ask the team for private key password)
+
+    2.3 Starting with MacOS 10.14.5, all signed applications by new `Developer ID Certificate` will need to be notarized. This is an automated step in the process. You'll need to enable notarization by setting the following env vars.
+
+    <pre>
+    export DESKTOP_APP_NOTARIZE=true
+    export DESKTOP_APP_APPLE_ID=VALID_APPLE_DEV_ID
+    export DESKTOP_APP_APPLE_PASSWORD=VALID_APPLE_DEV_PASSWORD
+    </pre>
+
+    2.4 Enable auto discovery for code signing process to pick up the certificates from the keychain
+
+    <pre>export CSC_IDENTITY_AUTO_DISCOVERY=true</pre>
+
+    2.5 Run release
+    <pre>npm run release</pre>
+
+    2.6 Validate if the app is signed with a `Developer ID Certificate` and notarized
+
+    <pre>spctl -a -t exec -v ./release/mac/Symbol\ Wallet.app
+    # Output(Success): ./release/mac/Symbol Wallet.app: accepted source=Notarized Developer ID
+    # Output(Failure): ./release/mac/Symbol Wallet.app: rejected source=Unnotarized Developer ID
+    </pre>
+
 ## Getting help
 
 Use the following available resources to get help:
@@ -64,7 +102,7 @@ Check [CONTRIBUTING](CONTRIBUTING.md) for information on how to contribute.
 
 ## License
 
-Copyright 2018-present NEM
+(C) Symbol Contributors 2022
 
 Licensed under the [Apache License 2.0](LICENSE)
 
--- scripts/notarize.js
+++ scripts/notarize.js
@@ -1,18 +1,29 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const {notarize} = require('electron-notarize')
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { notarize } = require('electron-notarize');
+const appBundleId = require('../package.json').build.appId;
 
-exports.default = async function notarizing(context) {
-  const {electronPlatformName, appOutDir} = context
+// You will need to notarize the application if the "Developer ID Certificate" is new
+exports.default = async (context) => {
+    const { electronPlatformName, appOutDir } = context;
+    if (electronPlatformName !== 'darwin') {
+        console.log('Skipping notarization because this is not a macOS build.');
+        return;
+    } else if (process.env.DESKTOP_APP_NOTARIZE !== 'true') {
+        console.log('Skipping notarization because DESKTOP_APP_NOTARIZE env is not set.');
+        return;
+    } else if (process.env.DESKTOP_APP_APPLE_ID === undefined || process.env.DESKTOP_APP_APPLE_PASSWORD === undefined) {
+        console.log('Skipping notarization because DESKTOP_APP_APPLE_ID or DESKTOP_APP_APPLE_PASSWORD env is not set.');
+        return;
+    }
+    const appName = context.packager.appInfo.productFilename;
+    const appPath = `${appOutDir}/${appName}.app`;
 
-  // Skip notarization when not on Mac OS.
-  if (electronPlatformName !== 'darwin') return
+    console.log(`Notarizing ${appName} with bundleId[${appBundleId}] at ${appPath} ... (This might take several minutes)`);
 
-  const appName = context.packager.appInfo.productFilename
-
-  return notarize({
-    appBundleId: 'io.symbol.desktop-wallet',
-    appPath: `${appOutDir}/${appName}.app`,
-    appleApiKey: process.env.APPLE_API_KEY_ID,
-    appleApiIssuer: process.env.APPLE_API_KEY_ISSUER_ID,
-  })
-}
+    return await notarize({
+        appBundleId: appBundleId,
+        appPath: appPath,
+        appleId: process.env.DESKTOP_APP_APPLE_ID,
+        appleIdPassword: process.env.DESKTOP_APP_APPLE_PASSWORD,
+    });
+};
